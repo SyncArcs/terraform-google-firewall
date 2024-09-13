1ee4d857da47449d38c5a501b6a9300192457763
ashishyadav83 <ashish@syncarc.net>
2024-09-13 18:07:01 +0530

    initial commit2

--- variable.tf
+++ variable.tf
@@ -81,4 +81,3 @@ variable "source_ranges" {
   default     = []
   description = "(Optional) If source ranges are specified, the firewall will apply only to traffic that has source IP address in these ranges."
 }
-
